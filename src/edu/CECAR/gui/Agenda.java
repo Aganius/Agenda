5a40e7fbc9e169a0cd891bc46212b19fbb871807
Johan Quintana <johangon4@hotmail.com>
2013-06-11 14:59:30 -0500

    #3 Pruebas

--- src/edu/CECAR/gui/Agenda.java
+++ src/edu/CECAR/gui/Agenda.java
@@ -1,7 +1,4 @@
-
-
 package edu.CECAR.gui;
-
 
 import javax.microedition.lcdui.Choice;
 import javax.microedition.lcdui.Command;
@@ -14,83 +11,76 @@ import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
 
 public class Agenda extends MIDlet implements CommandListener {
-        
-	private final Command comandoSalir = new Command("Salir", Command.EXIT, 0);
-	private final Command comandoSeleccionar = new Command("Seleccionar", Command.ITEM, 0);
-	private final Command comandoSesion = new Command("Sesion", Command.ITEM, 0);
-	private final Sesion sesion;
-	protected static Agenda ejemploMenu;
 
-	private List listaOpciones;
-	private Display display;
-	
-	private TextField textFiedUsuario;
-	private TextField textFieldPassword;
+    private final Command comandoSalir = new Command("Salir", Command.EXIT, 0);
+    private final Command comandoSeleccionar = new Command("Seleccionar", Command.ITEM, 0);
+    private final Command comandoSesion = new Command("Sesion", Command.ITEM, 0);
+    private final Sesion sesion;
+    protected static Agenda ejemploMenu;
+    private List listaOpciones;
+    private Display display;
+    private TextField textFiedUsuario;
+    private TextField textFieldPassword;
 
-	
-	
-	public Agenda() {
-		
-		configurarLista();
+    public Agenda() {
 
-                
-		listaOpciones.addCommand(comandoSalir);
-		listaOpciones.addCommand(comandoSeleccionar);
-		display = Display.getDisplay(this);
+        configurarLista();
 
-		ejemploMenu = this;
-		sesion = new Sesion(textFiedUsuario, textFieldPassword, comandoSesion, display, listaOpciones);
-                
 
-	}
+        listaOpciones.addCommand(comandoSalir);
+        listaOpciones.addCommand(comandoSeleccionar);
+        display = Display.getDisplay(this);
 
-	protected void destroyApp(boolean unconditional)
-			throws MIDletStateChangeException {
-		// TODO Auto-generated method stub
+        ejemploMenu = this;
+        sesion = new Sesion(textFiedUsuario, textFieldPassword, comandoSesion, display, listaOpciones);
 
-	}
 
-	protected void pauseApp() {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    protected void destroyApp(boolean unconditional)
+            throws MIDletStateChangeException {
+        // TODO Auto-generated method stub
+    }
 
-	protected void startApp() throws MIDletStateChangeException {
-		
-		display.setCurrent(sesion);
+    protected void pauseApp() {
+        // TODO Auto-generated method stub
+    }
 
-	}
+    protected void startApp() throws MIDletStateChangeException {
 
-	private void configurarLista() {
+        display.setCurrent(sesion);
 
-		listaOpciones = new List("Seleccionar Opciones", Choice.IMPLICIT);
-		listaOpciones.setCommandListener(this);
-		listaOpciones.append("Contactos", null);
-		listaOpciones.append("Tareas", null);
-		listaOpciones.append("Conversión de Monedas", null);               
-		
-	}
+    }
 
-	public void commandAction(Command c, Displayable d) {
-		// TODO Auto-generated method stub
+    private void configurarLista() {
 
-		if (c == comandoSalir) {
-			notifyDestroyed();
-		} else if ((d == listaOpciones && c == List.SELECT_COMMAND)
-				|| c == comandoSeleccionar) {
-			if (listaOpciones.getSelectedIndex() == 0) {
-				Contactos contactos = new Contactos(display, listaOpciones);
-				display.setCurrent(contactos);
-			} else if (listaOpciones.getSelectedIndex() == 1) {
-				Tareas tareas = new Tareas(display, listaOpciones);
-				display.setCurrent(tareas);
-			} else if (listaOpciones.getSelectedIndex() == 2) {
-				Conversiones conversiones = new Conversiones(display, listaOpciones);
-				display.setCurrent(conversiones);
-			}
+        listaOpciones = new List("Seleccionar Opciones", Choice.IMPLICIT);
+        listaOpciones.setCommandListener(this);
+        listaOpciones.append("Contactos", null);
+        listaOpciones.append("Tareas", null);
+        listaOpciones.append("Conversión de Monedas", null);
 
-		}
+    }
 
-	}
-	
+    public void commandAction(Command c, Displayable d) {
+        // TODO Auto-generated method stub
+
+        if (c == comandoSalir) {
+            notifyDestroyed();
+        } else if ((d == listaOpciones && c == List.SELECT_COMMAND)
+                || c == comandoSeleccionar) {
+            if (listaOpciones.getSelectedIndex() == 0) {
+                Contactos contactos = new Contactos(display, listaOpciones);
+                display.setCurrent(contactos);
+            } else if (listaOpciones.getSelectedIndex() == 1) {
+                Tareas tareas = new Tareas(display, listaOpciones);
+                display.setCurrent(tareas);
+            } else if (listaOpciones.getSelectedIndex() == 2) {
+                Conversiones conversiones = new Conversiones(display, listaOpciones);
+                display.setCurrent(conversiones);
+            }
+
+        }
+
+    }
 }
